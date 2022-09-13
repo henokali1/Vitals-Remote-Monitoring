c19f3f9bc492f39410ac08ebaa19cd3cf753e5fd
henokali1 <henokali1@gmail.com>
2022-09-13 18:19:40 +0400

    v0 starter

---
+++ src/Ardu/V0/V0.ino
@@ -0,0 +1,35 @@
+#include <Wire.h>
+#include <SparkFun_Bio_Sensor_Hub_Library.h>
+#include <SparkFunMLX90614.h>
+
+
+// No other Address options.
+#define DEF_ADDR 0x55
+
+// Reset pin, MFIO pin
+const int resPin = 4;
+const int mfioPin = 5;
+
+// Takes address, reset pin, and MFIO pin.
+SparkFun_Bio_Sensor_Hub bioHub(resPin, mfioPin); 
+IRTherm therm; // Create an IRTherm object to interact with throughout
+
+
+bioData body;  
+String heartrate_val = "";
+String body_temp_val = "";
+String ambient_temp_val = "";
+
+
+void setup(){
+  Serial.begin(115200);
+  Wire.begin();
+  tempSetup();
+  bioSensorSetup();
+}
+
+void loop(){
+  updateTemp();
+  updateHr();
+  delay(1000); // Slowing it down, we don't need to break our necks here.
+}
---
+++ src/Ardu/V0/helper.ino
@@ -0,0 +1,62 @@
+void tempSetup(){
+  if (therm.begin() == false){ // Initialize thermal IR sensor
+    Serial.println("Qwiic IR thermometer did not acknowledge! Freezing!");
+    while(1);
+  }
+  Serial.println("Qwiic IR Thermometer did acknowledge.");
+  
+  therm.setUnit(TEMP_C); // Set the library's units to Farenheit
+  // Alternatively, TEMP_F can be replaced with TEMP_C for Celsius or
+  // TEMP_K for Kelvin.
+  
+  pinMode(LED_BUILTIN, OUTPUT); // LED pin as output
+}
+
+void bioSensorSetup(){
+  int result = bioHub.begin();
+  if (!result)
+    Serial.println("Sensor started!");
+  else
+    Serial.println("Could not communicate with the sensor!!!");
+
+  Serial.println("Configuring Sensor...."); 
+  int error = bioHub.configBpm(MODE_ONE); // Configuring just the BPM settings. 
+  if(!error){
+    Serial.println("Sensor configured.");
+  }
+  else {
+    Serial.println("Error configuring sensor.");
+    Serial.print("Error: "); 
+    Serial.println(error); 
+  }
+  // Data lags a bit behind the sensor, if you're finger is on the sensor when
+  // it's being configured this delay will give some time for the data to catch
+  // up. 
+  delay(4000);   
+}
+
+
+void updateTemp(){
+  if (therm.read()) // On success, read() will return 1, on fail 0.
+  {
+    // Use the object() and ambient() functions to grab the object and ambient
+  // temperatures.
+  // They'll be floats, calculated out to the unit you set with setUnit().
+    body_temp_val = String(therm.object(), 2);
+    ambient_temp_val = String(therm.ambient(), 2);
+    Serial.print("Object: " + body_temp_val);
+    Serial.println("C");
+    Serial.print("Ambient: " + ambient_temp_val);
+    Serial.println("C");
+    Serial.println();
+  }
+}
+
+void updateHr(){
+  body = bioHub.readBpm();
+  if(body.confidence > 60){
+    heartrate_val = String(body.heartRate);
+    Serial.print("heartrate_val: ");
+    Serial.println(heartrate_val); 
+  }
+}
