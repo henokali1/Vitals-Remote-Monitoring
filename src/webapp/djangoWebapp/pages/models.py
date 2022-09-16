c8cda04f7cffac00454fdc4856ab618723a69cea
Ubuntu <ubuntu@ip-172-31-95-181.ec2.internal>
2022-09-16 13:22:32 +0000

    login/signup styling

--- src/webapp/djangoWebapp/pages/models.py
+++ src/webapp/djangoWebapp/pages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from time import time
+from django.utils import timezone
 
-# Create your models here.
+from django.db.models.fields import DateTimeField
+
+
+# Sensor Data
+class SensorData(models.Model):
+    heart_rate = models.FloatField(default=0.0)
+    body_temp = models.FloatField(default=0.0)
+    date = DateTimeField(default=timezone.now)
+    ts = models.IntegerField(default=0)
+
+    def __str__(self):
+        r = f'Date: {self.date} heart_rate: {self.heart_rate} - body_temp: {self.body_temp}'
+        return
